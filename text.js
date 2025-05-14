35b50635b795da35fd9c4f6e04b7c5650d3078ad
Petra-78 <partos.petra.0305@gmail.com>
2025-05-14 17:44:29 +0200

    initial commit + grid generation completed

---
+++ text.js
@@ -0,0 +1,19 @@
+const mainCont = document.querySelector(".main-container");
+
+// let gridSize = prompt("What size canvas do you want? (max. 100)");
+
+
+
+
+function makeRow(num) {
+    for (let i = 0 ; i < (num * num) ; i++) {
+        const gridBox = document.createElement("div");
+        const boxSize = 500 / num;
+        gridBox.className = "grid-box";
+        gridBox.style.height = boxSize + "px";
+        gridBox.style.width = boxSize + "px"
+        mainCont.appendChild(gridBox);
+    }
+}
+
+makeRow(16);
