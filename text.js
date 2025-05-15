7dba26ae996b69302402fca63630d611fcf93064
Petra-78 <partos.petra.0305@gmail.com>
2025-05-15 12:24:17 +0200

    added clear canvas button

--- text.js
+++ text.js
@@ -1,14 +1,10 @@
 const mainCont = document.querySelector(".main-container");
-
-// let gridSize = prompt("What size canvas do you want? (max. 100)");
-
-const gridBtn = document.createElement("button");
-gridBtn.className = "grid-btn";
-document.body.appendChild(gridBtn);
-gridBtn.textContent = "Select  grid size";
+const gridBtn = document.querySelector(".grid-btn");
 
 
-function makeRow(num) {
+function makeGrid(num) {
+    mainCont.innerHTML = "";
+
     for (let i = 0 ; i < (num * num) ; i++) {
         const gridBox = document.createElement("div");
         const boxSize = 500 / num;
@@ -19,12 +15,28 @@ function makeRow(num) {
     }
 }
 
-makeRow(16);
+gridBtn.addEventListener("click", () => {
+    let gridSize = prompt("What size canvas do you want? (max. 100)");
+    gridSize = +gridSize;
+    if (gridSize <= 100) {
+        makeGrid(gridSize);
+    } else {
+        alert("invalid input");
+    }
 
-const gridHover = document.querySelectorAll(".grid-box")
-gridHover.forEach(grid => {
-    grid.addEventListener ("mouseover", function() {
-        grid.style.backgroundColor = "black";
-    })
-})
+    const gridHover = document.querySelectorAll(".grid-box")
+        gridHover.forEach(grid => {
+            grid.addEventListener ("mouseover", function() {
+                grid.style.backgroundColor = "black";
+    });
+});
+});
 
+const clearBtn = document.querySelector(".clear-btn");
+
+clearBtn.addEventListener("click", () => {
+    const clearGrid = document.querySelectorAll(".grid-box");
+    clearGrid.forEach(grid => {
+        grid.style.backgroundColor = "rgb(214, 235, 236)";
+    });
+});
